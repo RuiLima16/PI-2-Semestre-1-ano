0ad66d126cae0a4c059a2cb9b3c6d676b22f1296
Rui Lima <98842074+RuiLima16@users.noreply.github.com>
2023-06-29 20:28:54 +0100

    Added Player Class

---
+++ PI/PI/src/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PI.src
+{
+    public class Player:Entity 
+    {
+        public override void Update()
+        {
+            
+        }
+        public override void Draw()
+        {
+            
+        }
+    }
+}
