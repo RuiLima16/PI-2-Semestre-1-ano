28fe81a25cf2adf553a1e88b208ea91f97a24d6e
Rui Lima <98842074+RuiLima16@users.noreply.github.com>
2023-06-29 20:27:00 +0100

    added entity class

--- PI/PI/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿
-using var game = new PI.Game1();
-game.Run();
---
+++ PI/PI/src/Entity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PI.src
+{
+    public abstract class Entity
+    {
+        Texture2D sprite;
+        Vector2 position;
+
+        public abstract void Update();
+
+        public abstract void Draw();
+    }
+}
